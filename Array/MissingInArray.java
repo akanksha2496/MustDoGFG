31250985f0a0676bdbc28b2d8da9812477c36a40
Akanksha Dewangan <59597850+akanksha2496@users.noreply.github.com>
2024-08-31 18:23:00 +0530

    Create MissingInArray.java

---
+++ Array/MissingInArray.java
@@ -0,0 +1,45 @@
+//https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] arr = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.missingNumber(n, arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, int arr[]) {
+
+        int totalN=(n*(n+1)/2);
+        int totalArr=0;
+        int i=0;
+        while(i<n-1){
+            totalArr+=arr[i];
+            i++;
+        }
+        return totalN-totalArr;
+        // Your Code Here
+    }
+}
