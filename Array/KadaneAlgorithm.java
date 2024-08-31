04be996e24dee989ecd9c01573123a58a4da2d8c
Akanksha Dewangan <59597850+akanksha2496@users.noreply.github.com>
2024-08-31 18:17:47 +0530

    Create KadaneAlgorithm.java

---
+++ Array/KadaneAlgorithm.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            Solution obj = new Solution();
+
+            // calling maxSubarraySum() function
+            System.out.println(obj.maxSubarraySum(arr));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+//https://www.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+// User function Template for Java
+class Solution {
+
+    // arr: input array
+    // Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int[] arr) {
+
+        // Your code here
+        int sum=Integer.MIN_VALUE,curr_sum=0;
+        for(int i=0;i<arr.length;i++){
+            if(curr_sum<0)
+            {
+                curr_sum=arr[i];
+            }
+            else
+            {curr_sum+=arr[i];}
+            sum=sum<curr_sum?curr_sum:sum;
+        }
+        return sum;
+    }
+}
